3110ece2f667f058a01f0e36e1689efc1f694100
Dom Dom <domizacz@gmail.com>
2020-08-06 15:48:15 -0500

    api sync

---
+++ DemoLabrary/ApiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLabrary
+{
+    public class ApiHelper
+    {
+        public static HttpClient APiClient { get; set; }
+    public static void InitializeClient()
+        {
+            APiClient = new HttpClient();
+            APiClient.DefaultRequestHeaders.Accept.Clear();
+            APiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+    }
+}
