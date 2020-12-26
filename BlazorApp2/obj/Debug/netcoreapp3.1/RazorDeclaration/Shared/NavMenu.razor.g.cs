7171b0c834b3423868d5a71491b5cb8e9da6b8fa
domizacz <domizacz@gmail.com>
2020-12-25 21:11:20 -0600

    secend work on captchav3

--- BlazorApp2/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ BlazorApp2/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -12,112 +12,112 @@ namespace BlazorApp2.Shared
     using System.Linq;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 1 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 2 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 3 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 4 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 5 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 6 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 7 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 8 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using BlazorApp2;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 9 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using BlazorApp2.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 10 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using SendGrid;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 11 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using SendGrid.Helpers.Mail;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 12 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using System.Threading.Tasks;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 13 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Mvc;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 14 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 14 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 15 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 15 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using Microsoft.Extensions.Logging;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 16 "Z:\Projekty\Test\BlazorApp2\_Imports.razor"
+#line 16 "Z:\Projekty\test\BlazorApp2\_Imports.razor"
 using System.ComponentModel.DataAnnotations;
 
 #line default
@@ -131,7 +131,7 @@ using System.ComponentModel.DataAnnotations;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "Z:\Projekty\Test\BlazorApp2\Shared\NavMenu.razor"
+#line 28 "Z:\Projekty\test\BlazorApp2\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
