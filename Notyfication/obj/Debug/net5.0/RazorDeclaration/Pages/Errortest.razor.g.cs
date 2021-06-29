1a9e192cb17e0344df60f70bc9cc2cc077bcecbe
Dom Dom <domizacz@gmail.com>
2021-06-29 13:06:33 -0500

    update

--- Notyfication/obj/Debug/net5.0/RazorDeclaration/Pages/Errortest.razor.g.cs
+++ Notyfication/obj/Debug/net5.0/RazorDeclaration/Pages/Errortest.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace BlazorApp1.Pages
+namespace Notyfication.Pages
 {
     #line hidden
     using System;
@@ -13,70 +13,76 @@ namespace BlazorApp1.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 1 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 2 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 3 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 4 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 5 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 6 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
+#line 7 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 8 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
-using BlazorApp1;
+#line 9 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
+using Notyfication;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Dom\Documents\Project\test\BlazorApp1\_Imports.razor"
-using BlazorApp1.Shared;
+#line 10 "C:\Users\Dom\Documents\Project\test\Notyfication\_Imports.razor"
+using Notyfication.Shared;
 
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.RouteAttribute("/")]
-    public partial class Index : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class Errortest : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
