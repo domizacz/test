b4cae1cf6cd554df3e86e1ba829143a3f37d9923
Dom Dom <domizacz@gmail.com>
2021-12-16 13:25:25 -0600

    Fir problem with the list add new sort of status

--- RemoteHand/Data/ClassRH.cs
+++ RemoteHand/Data/ClassRH.cs
@@ -4,6 +4,7 @@
     {
        public int id { get; set; }
        public string Location { get; set; }
+       public string Area { get; set; }
        public string Clinet { get; set; }
        public string Case { get; set; }
        public string Task { get; set; }
@@ -14,8 +15,8 @@
        public string Support { get; set; }
        public string Networking { get; set; }
        public string RemoteHand { get; set; }
-       public DateOnly Date_Added { get; set; }
-       public DateTime Data_work { get; set; }
+       public DateTime Date_Added { get; set; }
+       public DateOnly Data_work { get; set; }
 
 
 
@@ -24,13 +25,14 @@
     }
     public class Demo
     {
-        public List<ClassRH> DemoList()
+        public List<ClassRH> DemoRH()
         {
 
             var testdemo = new List<ClassRH>() {
-                new ClassRH(){ id=1, Location="NY4", Case="GTC-123451-1SA35D", Task="REPLACE HDD", Details="BROKEN HDD", Instruction="REMOVE OLD ADD NEW ONE TURN ONE THE SERVER", status="PENDING", Support="TEST SUPPORT", Networking="JB", RemoteHand="B612" },
-                new ClassRH(){ id=2, Location="NY4", Case="GTC-123451-1SA35D", Task="REPLACE HDD", Details="BROKEN HDD", Instruction="REMOVE OLD ADD NEW ONE TURN ONE THE SERVER", status="PENDING", Support="TEST SUPPORT", Networking="JB", RemoteHand="B612" }
-        };
+                new ClassRH(){ id=1, Clinet="GT",Location="NY4",Area="test" , Case="GTC-123451-1SA35D", Task="REPLACE HDD", Details="BROKEN HDD", Instruction="REMOVE OLD ADD NEW ONE TURN ONE THE SERVER", status="", Support="SUPPORT", Networking="JB", RemoteHand="B612", Date_Added=DateTime.Parse("10/15/2017"), Data_work=DateOnly.ParseExact("12/10/2017" , "MM/dd/yyyy") },
+                new ClassRH(){ id=2, Clinet="GT", Location="NY4", Area="test" Case="GTC-123451-1SA35D", Task="REPLACE HDD", Details="BROKEN HDD", Instruction="REMOVE OLD ADD NEW ONE TURN ONE THE SERVER", status="Tested", Support="SUPPORT", Networking="JB", RemoteHand="B612", Date_Added=DateTime.Parse("12/25/2017"), Data_work=DateOnly.ParseExact("12/15/2022" , "MM/dd/yyyy") },
+                new ClassRH(){ id=2, Clinet="BOBO", Location="NY4", Area="EAST COAST" Case="GTC-123451-1SA35D", Task="REPLACE HDD", Details="BROKEN HDD", Instruction="REMOVE OLD ADD NEW ONE TURN ONE THE SERVER", status="??", Support="SUPPORT", Networking="JB", RemoteHand="B612", Date_Added=DateTime.Parse("12/25/2017"), Data_work=DateOnly.ParseExact("12/15/2022" , "MM/dd/yyyy") }
+            };      
 
             return testdemo;
         }
