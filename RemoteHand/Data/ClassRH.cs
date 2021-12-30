b5394e7542af454e700277b1a9f352b7ac7bcda9
Dom Dom <domizacz@gmail.com>
2021-12-30 15:52:26 -0600

    working on sync

--- RemoteHand/Data/ClassRH.cs
+++ RemoteHand/Data/ClassRH.cs
@@ -2,27 +2,32 @@
 {
     public class ClassRH
     {
-       public int id { get; set; }
-       public string Location { get; set; }
-       public string Area { get; set; }
-       public string Clinet { get; set; }
-       public string Case { get; set; }
-       public string Task { get; set; }
-       public string Details { get; set; }
-       public string Instruction { get; set; }
-       public string status { get; set; }
-       public string tested { get; set; }
-       public string Support { get; set; }
-       public string Networking { get; set; }
-       public string RemoteHand { get; set; }
-       public DateTime Date_Added { get; set; }
-       public DateOnly Data_work { get; set; }
+        public int id { get; set; }
+        public string Location { get; set; }
+        public string Area { get; set; }
+        public string Clinet { get; set; }
+        public string Case { get; set; }
+        public string Task { get; set; }
+        public string Details { get; set; }
+        public string Instruction { get; set; }
+        public string status { get; set; }
+        public string tested { get; set; }
+        public string Support { get; set; }
+        public string Networking { get; set; }
+        public string RemoteHand { get; set; }
+        public DateTime Date_Added { get; set; }
+        public DateOnly Data_work { get; set; }
 
 
 
-        
+
 
     }
+    public class Time
+    {
+    public DateOnly friday { get; set; }
+    }
+
     public class Demo
     {
         public List<ClassRH> DemoRH()
