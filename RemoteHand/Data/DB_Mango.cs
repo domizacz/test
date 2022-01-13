f0803e1deb535572dcd295cd7b454eb23d0f0e7a
Dom Dom <domizacz@gmail.com>
2022-01-13 16:00:24 -0600

    working on request

--- RemoteHand/Data/DB_Mango.cs
+++ RemoteHand/Data/DB_Mango.cs
@@ -4,7 +4,13 @@ namespace RemoteHand.Data
 {
     public class DB_Mango
     {
-        
+       
+        string db_mongo_key = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+        var client = new MongoClient(db_mongo_key);
+        var database = client.GetDatabase("test");
+        var collection = database.GetCollection<ClassRH>("test");
+
+        await collection.InsertManyAsync(temp.DemoRH());
 
 
     }
